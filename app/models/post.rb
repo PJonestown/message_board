e4fd1515a5ab998fea915b22c956a47961918b21
Paul Johnston <anthpaul0@gmail.com>
2015-02-20 15:09:11 -0600

    presence and length post validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
 	validates :title, :body, presence: true
+	validates :title, :length=>{maximum:140}
+	validates :body, :length=>{maximum:5000}
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:post) do
+  	Post.new(title: 'Foo', body: 'foobar')
+	end
+
+	# use describe and context to clean
+
+	it "should be valid" do 
+		expect(post).to be_valid
+	end
+
+	it "should be invalid if its title is blank" do 
+		post.title = nil
+		expect(post).not_to be_valid
+	end
+
+	it "should be invalid if its body is blank" do 
+		post.body = nil
+		expect(post).not_to be_valid
+	end
+
+	it "should be invalid if the title is longer than 140 characters" do 
+		post.title = 'a' * 141
+		expect(post).not_to be_valid
+	end
+
+	it "should be invalid if body is larger that 5000" do
+		post.body = 'a' * 5001
+		expect(post).not_to be_valid
+	end
+
+
+
 end
